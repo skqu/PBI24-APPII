923a42879f57f9e5983d1d8f62bf62f0db3bf73e
Stefan Quvang Christensen <SKQU@iba.dk>
2025-03-10 10:38:26 +0100

    Added code example and UML diagrams

---
+++ UML/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+class Alice
+{
+    static void Main()
+    {
+        Everything smt = new Everything();
+        if (smt.Bob() == 0)
+        {
+            Console.WriteLine("Everything is fine");
+        }else if(smt.Bob() == 1)
+        {
+            smt.Log("start");
+            for (int i = 0; i < 1000; i++)
+            {
+                smt.Bob();
+            }
+            smt.Log("Stop");
+        }else
+        {
+            Console.WriteLine("Repeat");
+        }
+    }
+}
+
+class Everything
+{
+
+    public byte Bob()
+    {
+        Random rnd = new Random();
+        byte min = 0, max = 5;
+        byte number = (byte) rnd.Next(min, max);
+        return number;
+    }
+
+    public void Log(string msg)
+    {
+        Console.WriteLine("Log entry with msg: " + msg);
+    }
+}
